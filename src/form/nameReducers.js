e8d35cf9f04c9f36018fbd1146f6a4ee09f46199
diren-ice <dirennessd@gmail.com>
2023-06-02 13:46:29 +0600

    Создала кнопку delete для todos

--- src/form/nameReducers.js
+++
@@ -1,8 +0,0 @@
-export const nameReducer = (state={name: ''}, action) => {
-  if (action.type === 'CHANGE_NAME') {
-      return {...state, name: action.payload}
-      } return state
-}
-const CHANGE_NAME = 'CHANGE_NAME'
-
-export const changeName = payload => ({type: CHANGE_NAME, payload})
